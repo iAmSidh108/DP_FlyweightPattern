925fbada30cd680e4434a942cb1a994440012a77
Siddharth Tiwari <sidh589@gmail.com>
2024-02-26 23:19:35 +0530

    Created script to generate 20*20 cubes in the scene on start and checked the performance.

---
+++ Dev_FlyweightPattern/Assets/Data/Example1/CreateWorldPrefab.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWorldPrefab : MonoBehaviour
+{
+
+    public int width;
+    public int depth;
+    public GameObject cube;
+    
+    void Start()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for(int z = 0; z < depth; z++)
+            {
+                Vector3 pos = new Vector3(x, Mathf.PerlinNoise(x*.2f, z *.2f) * 3, z);
+                Instantiate(cube, pos, Quaternion.identity);
+            }
+        }
+    }
+
+    
+}
